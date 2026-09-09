56b09fd6e44af90a29cd46a0397352030c4c1055
Lakshmipriya J <lakshmipriyajaya1408@gmail.com>
2026-09-09 16:01:50 +0530

    Prepare backend for cloud deployment

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/sweet_delights_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
